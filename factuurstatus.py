f73de9cdcc0cb462a1d93e558f10d5b083fb5f87
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 18:17:01 +0200

    Create factuurstatus.py

---
+++ factuurstatus.py
@@ -0,0 +1,12 @@
+# factuurstatus.py
+
+def get_factuurstatussen():
+    return [
+        "te factureren",
+        "gefactureerd",
+        "voldaan",
+        "oninbaar"
+    ]
+
+def default_status():
+    return "te factureren"
