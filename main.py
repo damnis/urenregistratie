1a4705dc0edd669340d606c52c4963db94d3e85c
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 18:29:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,34 @@ from uren import invoer_uren
 from overzicht import toon_overzicht
 from facturen import genereer_factuur
 from instellingen import instellingen_menu
-from database import init_db
-init_db()
 
-st.image("https://www.kage.nl/newcms/wp/wp-content/uploads/cropped-Kage-ConsultLogo300.png", width=300)
+# Zet pagina-instellingen
+st.set_page_config(page_title="Urenregistratie & Facturatie", layout="wide")
 
+# Toon logo bovenaan
+st.image(
+    "https://www.kage.nl/newcms/wp/wp-content/uploads/cropped-Kage-ConsultLogo300.png",
+    width=300,
+    use_column_width=False
+)
 
-st.set_page_config(page_title="Uren & Facturatie", layout="wide")
+# Titel van de app (optioneel)
+st.title("🧾 Urenregistratie en Facturatie App")
 
-menu = st.sidebar.selectbox("Menu", ["Uren invoer", "Overzicht", "Factuur maken", "Instellingen"])
+# Sidebar-menu
+pagina = st.sidebar.selectbox("📂 Kies een pagina", [
+    "Uren invoer",
+    "Overzicht",
+    "Factuur maken",
+    "Instellingen"
+])
 
-if menu == "Uren invoer":
+# Paginaweergave
+if pagina == "Uren invoer":
     invoer_uren()
-elif menu == "Overzicht":
+elif pagina == "Overzicht":
     toon_overzicht()
-elif menu == "Factuur maken":
+elif pagina == "Factuur maken":
     genereer_factuur()
-elif menu == "Instellingen":
+elif pagina == "Instellingen":
     instellingen_menu()
