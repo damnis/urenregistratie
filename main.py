f80d06f16fb785d416ceda17a0c68c2aa445d744
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 15:42:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from uren import invoer_uren
 from overzicht import toon_overzicht
 from facturen import genereer_factuur
 from instellingen import instellingen_menu
+from database import init_db
+init_db()
 
 st.set_page_config(page_title="Uren & Facturatie", layout="wide")
 
