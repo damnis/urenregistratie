c0caef11874687831ceec05b418c89e54bfcb809
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 15:33:42 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from uren import invoer_uren
+from overzicht import toon_overzicht
+from facturen import genereer_factuur
+from instellingen import instellingen_menu
+
+st.set_page_config(page_title="Uren & Facturatie", layout="wide")
+
+menu = st.sidebar.selectbox("Menu", ["Uren invoer", "Overzicht", "Factuur maken", "Instellingen"])
+
+if menu == "Uren invoer":
+    invoer_uren()
+elif menu == "Overzicht":
+    toon_overzicht()
+elif menu == "Factuur maken":
+    genereer_factuur()
+elif menu == "Instellingen":
+    instellingen_menu()
