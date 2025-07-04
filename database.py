690847091d15345ed7864d314d068e422bcca41a
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 18:47:12 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,8 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("facturatie.db")
     c = conn.cursor()
-
     c.execute('''
-       CREATE TABLE IF NOT EXISTS uren (
+        CREATE TABLE IF NOT EXISTS uren (
             id INTEGER PRIMARY KEY,
             medewerker TEXT,
             klant TEXT,
@@ -14,11 +13,9 @@ def init_db():
             starttijd TEXT,
             eindtijd TEXT,
             uren REAL,
-            omschrijving TEXT
+            omschrijving TEXT,
             factuurstatus TEXT
-        ) 
-        
+        )
     ''')
-
     conn.commit()
     conn.close()
