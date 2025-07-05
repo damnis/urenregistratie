b181674dec675860cd26d3c1f971a5caea214764
damnis <36800988+damnis@users.noreply.github.com>
2025-07-05 17:30:10 +0200

    Create prijsafspraak.py

---
+++ prijsafspraak.py
@@ -0,0 +1,15 @@
+def get_prijsafspraken():
+    return {
+        "klant1": {
+            "prijs": 500.00,
+            "uitsluiten": ["810", "820", "899"]  # of beginnend met '8'
+        },
+        "klant2": {
+            "prijs": 750.00,
+            "uitsluiten": ["820"]
+        }
+    }
+
+def get_vaste_prijs(klant):
+    afspraken = get_prijsafspraken()
+    return afspraken.get(klant, None)
